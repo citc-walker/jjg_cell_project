e18d57f829b874b61903eca74ad1459dd3e512de
citc-walker <75872281+citc-walker@users.noreply.github.com>
2022-07-14 13:33:55 +0900

    Update Ribosome.java

--- Ribosome.java
+++ Ribosome.java
@@ -1,7 +1,5 @@
 package jjg.cellproject.citcwalker;
 
-import java.util.ArrayList;
-
 public class Ribosome {//리보솜 클래스
 	private String[] codon;
 	private String index;
@@ -9,8 +7,8 @@ public class Ribosome {//리보솜 클래스
 			'ㅅ','ㅆ','ㅇ','ㅈ','ㅉ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ'};
 	private final char[] JOONG_C = {'ㅏ','ㅐ','ㅑ','ㅒ','ㅓ','ㅔ','ㅕ','ㅖ','ㅗ','ㅘ',
 			'ㅙ','ㅚ','ㅛ','ㅜ','ㅝ','ㅞ','ㅟ','ㅠ','ㅡ','ㅢ','ㅣ'};
-	private final char[] JONG_C = {'`','ㄱ','ㄲ','ㄳ','ㄴ','ㄵ','ㄶ','ㄷ','ㄹ','ㄺ','ㄻ','ㄼ',
-			'ㄽ','ㄾ','ㄿ','ㅀ','ㅁ','ㅂ','ㅄ','ㅅ','ㅆ','ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ'};
+	private final char[] JONG_C = {'`','ㄱ','ㄲ','ㄳ','ㄴ','ㄵ','ㄶ','ㄷ','ㄹ'
+			,'ㅁ','ㅂ','ㅅ','ㅆ','ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ'};
 	private final String[] JONG = {"","ㄱ","ㄲ","ㄳ","ㄴ","ㄵ","ㄶ","ㄷ","ㄹ","ㄺ","ㄻ","ㄼ",
 			"ㄽ","ㄾ","ㄿ","ㅀ","ㅁ","ㅂ","ㅄ","ㅅ","ㅆ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"};
 	private final String[][] CODON = {{"AAC"}, {"AAG"}, {"AAU", "AAA"}, {"CAC"}, {"CAG"}, 
@@ -23,9 +21,39 @@ public class Ribosome {//리보솜 클래스
 	private final String[] data = {"ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ",
 			"ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ",
 		"`", ".", ",", "!", "?", " ", "(", ")", "~", "-", "@", "#", "$", "%"};
+	private final char[] data_c = {'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ',
+			'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ',
+		'`', '.', ',', '!', '?', ' ', '(', ')', '~', '-', '@', '#', '$', '%'};
+	public Ribosome() {
+		
+	}
 	public Ribosome(String codon) {//리보솜 객체가 선언됐을때 ㅗ ㅘ ㅙ ㅚ
 		this.codon = codon.split("(?<=\\G...)");
 		this.index = interpret(this.codon);
+	}
+	public String interpret(String codonl) {//코돈을 한글로
+		String[] codonlist = codonl.split("(?<=\\G...)");
+		String result="", sent = "";
+		for(int i = 0; i < codonlist.length; i++) {//어떤 글자인지 검출
+			for(int j = 0; j < CODON.length; j++) {
+				for(int k = 0; k < CODON[j].length; k++) {
+					if(CODON[j][k].equals(codonlist[i])) {
+						result += data[j]+"";
+					}
+				}
+			}
+		}//유니코드 산출 공식 (int to String) (초성 * 21 + 중성) * 28 + 종성 + 0xAC00
+		String[] k = result.split("(?<=\\G...)");
+		for(int i = 0; i < k.length; i++) {
+			char[] t = k[i].toCharArray();
+			int cho, jun, jong;
+			cho = findIndex(CHO_C, t[0]);
+			System.out.println(t[0]);
+			jun = findIndex(JOONG_C, t[1]);
+//			jong = findIndex(JONG_C, t[2]);
+			sent += (char)((cho * 21 + jun) * 28 /*+ jong */+ 0xAC00);
+		}
+		return sent;
 	}
 	public String interpret(String[] codonlist) {//코돈을 한글로
 		String result="", sent = "";
@@ -70,23 +98,28 @@ public class Ribosome {//리보솜 클래스
 		}
 		return t;
 	}
-	public String translate(String sentence) {
+	public String translate(String sentence) { //한글을 코돈으로
 		String result = "";
 		char[] s_c = sentence.toCharArray();
 		for(int i = 0; i < s_c.length; i++) {
-			char cho = (char)((String.valueOf(s_c[i]).charAt(0)-0xAC00)/28/21);
-			char jung = (char)((String.valueOf(s_c[i]).charAt(0)-0xAC00)/28%21);
-			char jong = (char)((String.valueOf(s_c[i]).charAt(0)-0xAC00)%28);
-			String j;
-			if(String.valueOf(jong) == JONG[0]) {
-				j = "";
-			}else {
-				j = String.valueOf(jong);
-			}
-			System.out.println(cho);
-			result+=CODON[findIndex(CHO_C, cho)][0];
-			result+=CODON[findIndex(JOONG_C, jung)][0];
-			result+=CODON[findIndex(JONG, j)][0];
+			char cho = CHO_C[(int)((String.valueOf(s_c[i]).charAt(0)-0xAC00)/28/21)];
+			char jung = JOONG_C[(int)((String.valueOf(s_c[i]).charAt(0)-0xAC00)/28%21)];
+//			String jong = "";
+//			System.out.println((char)((s_c[i]-0xAC00)%28)+1);
+//			if(String.valueOf((s_c[i]-0xAC00)%28) == null) {
+//				jong = data[40];
+//			}else {
+//				jong= JONG[((s_c[i]-0xAC00)%28)];
+//			}
+//			String j;
+//			if(jong == JONG[0]) {
+//				j = "";
+//			}else {
+//				j = String.valueOf(jong);
+//			}
+			result+=CODON[findIndex(data_c, cho)][0];
+			result+=CODON[findIndex(data_c, jung)][0];
+//			result+=CODON[findIndex(JONG, j)][0];
 		}
 		return result;
 	}
